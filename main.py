3593772c81bb8bb729b10698b3780f6dce7076bb
Miilenna <milena200.fv@gmail.com>
2025-01-13 18:22:07 +0100

    exercici 4 fet

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import List, Optional
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import create
+import read
+from schema import users_registre
 app = FastAPI()
 
 class registre(BaseModel):
@@ -15,7 +17,7 @@ class registre(BaseModel):
     codigo_postal: Optional[int]
     contraseña: str
 
-@app.post("/usuari/add", response_model=List[dict])
+@app.post("/usuari/add", response_model=dict)
 async def add(usuari: registre):
     resultat = create.add_user(usuari.nombre, usuari.apellido, usuari.correo, usuari.descripción, usuari.curso, usuari.año, usuari.dirección, usuari.codigo_postal, usuari.contraseña)
     if resultat.get("status") !=1:
@@ -23,4 +25,9 @@ async def add(usuari: registre):
     return {
         "message":"Afegit correctament"
     }
-
+@app.get("/usuari/get", response_model=dict)
+async def get():
+    resultat = read.read_user()
+    if not resultat:
+        raise HTTPException(status_code=400, detail="No s'ha pogut fer el insert")
+    return users_registre(resultat)
